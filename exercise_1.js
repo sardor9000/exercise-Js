c67be5ccb6bb2cbbcab858d3764c1ab97daf8c24
Sardor <usok201802@gmail.com>
2023-07-06 13:35:38 +0900

    functions

---
+++ exercise_1.js
@@ -0,0 +1,109 @@
+
+
+
+
+
+// let temp = 'cold';
+
+// if(temp === 'hot'){
+//     console.log('The temperature is hot');
+// }else if(temp === 'it is cold'){
+//     console.log('The temperature is cold');
+// }else if(temp === 'rain'){
+//     console.log('it is raining');
+// }else if(temp === 'snow'){
+//     console.log('it is snowing');
+// }else{
+//     console.log('unknown weather');
+// }
+
+// if (temp === 'sovuq'){
+//     console.log('unknown weather');
+// }
+
+
+// while, do while
+
+// let i = 0;
+// 
+// while(i < 10){
+//     console.log(i);
+//     i++
+// }
+
+// for(let i = 0; i <= 2; i++){
+//     console.log('--out loop');
+// 
+//     for(let j = 0; j <= 3; j++){
+//         console.log('------------inner loop');
+//     }
+// }
+
+
+// console.log(Number.isInteger(num));
+// console.log(typeof Number.parseInt(num));
+// console.log(typeof Number.parseInt(num));
+// console.log(typeof +num);
+// console.log(parseInt(num.toFixed(3)));
+
+// let num = 10.234564562;
+
+
+// Math built in methods
+
+// console.log(Math.pow(3,2));
+// onsole.log(Math.sqrt(25));
+// console.log(Math.sqrt(25));
+// console.log(Math.max(64,43,1,42,5));
+// console.log(Math.min(64,43,1,42,5));
+// console.log(Math.abs(-100));
+// console.log(parseInt(Math.random() * 10));
+// console.log(Math.round(10.5));
+// console.log(Math.floor(10.6));
+// console.log(Math.ceil(10.01));
+
+
+
+
+
+// function => Decleration, Expression, Arrow
+
+// Decleration
+// function test(){
+//     console.log('Salom')
+// }
+// test()
+
+
+// Expresson 
+
+// let test = function(){
+//     console.log(10+10);
+// }
+// test()
+ 
+
+// Arrow
+
+// let test = () => 10+10
+    
+// console.log(test());
+
+
+// function person(name, status, age){
+//     return 'Hello' + ' ' + name + ' ' + status +' ' + age
+// }
+// 
+// console.log(person('John', 'Developer', 25));
+
+
+
+// Default parametr
+
+// function person(name = 'Sardor'){
+//     console.log('Hello' + ' ' + name);
+// }
+// 
+// person()    // Hello Sarddor
+
+
